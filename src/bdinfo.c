4b534fbb02db15a32a4a21db80a60785ef10e88b
schnusch <schnusch@users.noreply.github.com>
2018-05-17 12:55:39 +0200

    fixed version

--- src/bdinfo.c
+++ src/bdinfo.c
@@ -17,7 +17,7 @@ You should have received a copy of the GNU Lesser General Public License
 along with bdinfo.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#define BDINFO_VERSION  "?.?.?"
+#define BDINFO_VERSION  "1.4.1"
 #define BLURAY_SPELLING "Blu-ray"
 
 #define _GNU_SOURCE
